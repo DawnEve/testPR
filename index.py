5ff5180527b40c804aed629004944c98ce980c7e
jimmy <JimmyMall@live.com>
2020-10-10 21:56:40 +0800

    v0.3 in my notebook

--- index.py
+++ index.py
@@ -4,14 +4,18 @@ def cub(n):
 # test
 print( cub(5) )
 
+def add():
+	pass
 
 def calculate(n):
     i=0
+    j=12
     for index in range(n):
         i=i+index
     print (i)
 calculate(100)
 
 def minus(a,b):
+	# this is a test 
     return a-b
 
